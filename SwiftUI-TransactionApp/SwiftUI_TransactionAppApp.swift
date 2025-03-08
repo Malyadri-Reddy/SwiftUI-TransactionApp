46e65c4bbe9240f7d452e182fe1857ebdf312010
Malyadri <malyadri.marthula@abbott.com>
2025-03-08 15:47:46 +0530

    Initial Commit

---
+++ SwiftUI-TransactionApp/SwiftUI_TransactionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TransactionAppApp.swift
+//  SwiftUI-TransactionApp
+//
+//  Created by Malyadri Reddy on 08/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TransactionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-TransactionAppTests/SwiftUI_TransactionAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TransactionAppTests.swift
+//  SwiftUI-TransactionAppTests
+//
+//  Created by Malyadri Reddy on 08/03/25.
+//
+
+import Testing
+@testable import SwiftUI_TransactionApp
+
+struct SwiftUI_TransactionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
